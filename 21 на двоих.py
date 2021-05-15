ac066894436d254df8fec88ae801fc9d55cdba57
Goshanbl41 <84232599+Goshanbl41@users.noreply.github.com>
2021-05-15 15:27:31 +0300

    123
    Qwe

---
+++ 21 на двоих.py
@@ -0,0 +1,47 @@
+koloda = [2,3,4,5,6,7,8,9,10,11] * 4
+import random
+random.shuffle(koloda)
+print (" Поиграем в 21")
+count1=0
+count2=0
+current1 = koloda.pop()
+count1 += current1
+current2 = koloda.pop()
+count2 += current2
+current1 = koloda.pop()
+count1 += current1
+current2 = koloda.pop()
+count2 += current2
+print('У Вас %d' %count1)
+print('У Вашего аппонента %d' %count2)
+while True:
+    choice = input('Игрок1 Будете брать карту? y/n')
+    if choice == 'y':
+        current1 = koloda.pop()
+        print('Вам попалась карта достоинством %d' %current1)
+        count1 += current1
+        
+    elif choice == 'n':
+            print('У Игрок1 %d очков.' %count1)
+            break
+while True:
+    choice = input('Игрок2 Будете брать карту? y/n')
+    if choice == 'y':
+        current2 = koloda.pop()
+        print('Вам попалась карта достоинством %d' %current2)
+        count2 += current2
+        if count2 > 21:
+            print('выйграл игрок1')
+    elif choice == 'n':
+            print('У Игрок2 %d очков.' %count2)
+            break
+if count1 > 21:
+    print('выйграл игрок2')
+if count2 > 21:
+    rint('выйграл игрок1')
+if count1 > count2:
+    print('Выйграл Игрок1')
+if count1 < count2:
+    print('Выйграл Игрок2')
+if count1 == count2:
+    print('Ничья')
